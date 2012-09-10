7beccf89a5a26ac26fe61f7ccc4c12eca90f7b51
Edward Simpson <edsimpson@gmail.com>
2012-09-10 15:35:23 -0700

    Another minimum viable product.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mvp2::Application.config.secret_token = 'c00622ad01f871609998b1fe3a615668cfe57eb2da29c353ddce7f65a8e25d02b1c58260dae8a7b35917e5cc6f3e899bed97da723bf2ab6a05d5bd1e3fa001b4'
