74fa7f01e5ae6b9eeb12f650b887021979b1a8fe
Edward Simpson <edsimpson@gmail.com>
2012-09-22 19:30:24 -0700

    Start view specs for home page.
    Baby specs for the home page.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-	def log_in_out_link
+	def sign_in_out_link
 		if user_signed_in?
-			link_to 'Logout', destroy_user_session_path, :method => :delete
+			link_to 'Logout', destroy_user_session_path, method: :delete, class: 'sign_out'
 		else
-			link_to 'Login', new_user_session_path
+			link_to 'Login', new_user_session_path, class: 'sign_in'
 		end
 	end
 	
@@ -11,7 +11,7 @@ module ApplicationHelper
 		if user_signed_in?
 			"Hello, #{current_user.email}"
 		else
-			link_to 'Join us!', new_user_registration_path
+			link_to 'Join us!', new_user_registration_path, class: 'sign_up'
 		end
 	end
 end
--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
-describe "home/index.html.haml" do
+describe "home/index" do
 	it "displays the home page messaging" do
 		render
 		rendered.should contain("Get Answers")
+	end
+	
+	it "displays a sign-up link" do
+		render template: 'home/index', layout: 'layouts/application'
+		rendered.should have_selector('a.sign_up')
+	end
+	
+	it "displays a sign-in link" do
+		render template: 'home/index', layout: 'layouts/application'
+		rendered.should have_selector('a.sign_in')
 	end
 end
