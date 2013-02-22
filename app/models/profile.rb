eeb33ea4f146520948173a50c9510966cb5344bf
Edward Simpson <edsimpson@gmail.com>
2013-02-21 22:16:54 -0800

    Validation tweak for profile invitation email address.
    To only validate invitation_email if present, use allow_blank option
    instead of evaluating a test string.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -27,7 +27,7 @@ class Profile < ActiveRecord::Base
 	# validates :categories, length: {maximum: 1}
 	validates :availability, :awards, :education, :experience, :insurance_accepted, :rates, :summary, 
 		:office_hours, :phone_hours, :video_hours, :admin_notes, length: {maximum: 1000}
-	validates :invitation_email, email: true, if: 'invitation_email.present?'
+	validates :invitation_email, email: true, allow_blank: true
 	validates :primary_phone, phone_number: true, allow_blank: true
 	validates :secondary_phone, phone_number: true, allow_blank: true
 	
