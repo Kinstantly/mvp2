697836e7211cbef7547e14d6722e335d84b54bb2
Edward Simpson <edsimpson@gmail.com>
2013-02-25 21:20:38 -0800

    Populated associated services and specialties for the attorneys category.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130221022449) do
+ActiveRecord::Schema.define(:version => 20130226030907) do
 
   create_table "age_ranges", :force => true do |t|
     t.string   "name"
