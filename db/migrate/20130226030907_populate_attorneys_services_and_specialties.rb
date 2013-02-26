149e9a7a1e8105c73b89e2194c93fff300dfe949
Edward Simpson <edsimpson@gmail.com>
2013-02-25 21:21:57 -0800

    Migration file for services and specialties for attorneys category.

---
+++ db/migrate/20130226030907_populate_attorneys_services_and_specialties.rb
@@ -0,0 +1,87 @@
+class PopulateAttorneysServicesAndSpecialties < ActiveRecord::Migration
+	class Category < ActiveRecord::Base
+		has_and_belongs_to_many :services
+	end
+
+	class Service < ActiveRecord::Base
+		has_and_belongs_to_many :categories
+		has_and_belongs_to_many :specialties
+	end
+
+	class Specialty < ActiveRecord::Base
+		has_and_belongs_to_many :services
+	end
+
+	def up
+		Category.reset_column_information
+		Service.reset_column_information
+		Specialty.reset_column_information
+		parse_lines.each do |cat_name, svcs|
+			cat = predefine(cat_name.to_category)
+			svcs.each do |svc_name, specs|
+				svc = predefine(svc_name.to_service)
+				cat.services << svc unless cat.services.include?(svc)
+				specs.each do |spec_name|
+					spec = predefine(spec_name.to_specialty)
+					svc.specialties << spec unless svc.specialties.include?(spec)
+				end
+			end
+		end
+	end
+
+	# Leave categories in place.
+	def down
+		Category.reset_column_information
+		Service.reset_column_information
+		Specialty.reset_column_information
+		parse_lines.each do |cat_name, svcs|
+			svcs.each do |svc_name, specs|
+				specs.each do |spec_name|
+					spec_name.to_specialty.destroy
+				end
+				svc = svc_name.to_service
+				svc.destroy if svc.specialties.blank?
+			end
+			# cat = cat_name.to_category
+			# cat.destroy if cat.services.blank?
+		end
+	end
+	
+	def parse_lines
+		load_lines
+		names = {}
+		@lines.strip.split("\n").each do |line|
+			cat, svc, specs, comments = line.strip.split(';')
+			cat.strip!
+			svc.strip!
+			names[cat] ||= {}
+			names[cat][svc] ||= []
+			# Split specialty names on commas. Ignore and remove escaped commas.
+			specs.strip.split(/(?<!\\),/).each do |spec|
+				spec.strip!
+				spec.delete!("\\")
+				names[cat][svc] << spec unless names[cat][svc].include?(spec)
+			end if specs
+		end
+		names
+	end
+	
+	def predefine(record)
+		unless record.is_predefined
+			record.is_predefined = true
+			record.save
+		end
+		record
+	end
+	
+	def load_lines
+		@lines = <<'eof'
+ATTORNEYS;adoption attorneys;agency adoption, independent adoption, domestic partner adoptions, international adoption, single parent adoption, stepparent adoption;
+ATTORNEYS;family law attorneys;appeals, bullying, child custody, child support, civil rights litigation, conservatorship, divorce, domestic violence, expulsions, guardianship, juvenile court proceedings, special education, special needs trusts, suspensions;
+ATTORNEYS;special needs attorneys;appeals, disciplinary matters, consultations, due process hearings, Individual Education Placement (IEP) meetings, mediation, public school placements, private placements through public schools;
+ATTORNEYS;juvenile law attorneys;appeals, juvenile court proceedings;
+ATTORNEYS;child support collection attorneys;appeals;
+ATTORNEYS;child custody attorneys;appeals, custody disputes, legal custody, physical custody;
+eof
+	end
+end
