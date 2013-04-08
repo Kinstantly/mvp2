9a1e0885b7e9212d732611ab5e7759129253ffaa
Edward Simpson <edsimpson@gmail.com>
2013-04-07 18:24:36 -0700

    Integrate HTML and CSS for pretty home page.
    Restrict CSS files used by application.css for the plain look.
    Use ERB index file for first cut of pretty home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
 	# before_filter :authenticate_user!
 	# layout 'no_user_registration'
+	layout false, only: :index
+	
+	def index
+		render handlers: [:erb]
+	end
 end
