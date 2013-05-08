a8565898b515dec4b753557e07a696ed314a1e06
Edward Simpson <edsimpson@gmail.com>
2013-05-07 23:10:27 -0700

    Created age range for adoption.
    Adoption is listed before other age ranges.

---
+++ db/migrate/20130508054800_create_adoption_age_range.rb
@@ -0,0 +1,23 @@
+class CreateAdoptionAgeRange < ActiveRecord::Migration
+	class AgeRange < ActiveRecord::Base
+		attr_accessible :name, :sort_index
+	end
+	
+	def up
+		AgeRange.reset_column_information
+		AgeRange.all.each do |age_range|
+			age_range.sort_index += 1
+			age_range.save!
+		end
+		AgeRange.create(name: 'adoption', sort_index: 1)
+	end
+	
+	def down
+		AgeRange.reset_column_information
+		AgeRange.find_by_name('adoption').destroy
+		AgeRange.all.each do |age_range|
+			age_range.sort_index -= 1
+			age_range.save!
+		end
+	end
+end
