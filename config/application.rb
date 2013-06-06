10aeb9b5bf43e7ff933c86ed7fe91fb56ebbcbcd
Edward Simpson <edsimpson@gmail.com>
2013-06-05 17:27:47 -0700

    Changed default email sender address to include our name.

--- config/application.rb
+++ config/application.rb
@@ -79,7 +79,8 @@ end
 
 DEFAULT_REGION = 'CA'
 DEFAULT_COUNTRY_CODE = 'US'
-MAILER_DEFAULT_FROM = 'support@kinstantly.com'
+MAILER_DEFAULT_FROM = 'Kinstantly <no-reply@kinstantly.com>'
+MAILER_DEFAULT_REPLY_TO = 'Kinstantly <support@kinstantly.com>'
 MAILER_DEFAULT_BCC = 'monitor@kinstantly.com'
 Phonie::Phone.default_country_code = '1'
 SEARCH_DEFAULT_PER_PAGE = ENV['SEARCH_DEFAULT_PER_PAGE'].presence || 20
