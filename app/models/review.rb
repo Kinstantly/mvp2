b662ace44708c070a19d612a172ad28053f75cda
Edward Simpson <edsimpson@gmail.com>
2013-08-13 18:00:11 -0700

    Limit the length of the review body attribute.
    Implemented with support for length validation of future text and
    string attributes.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,12 @@ class Review < ActiveRecord::Base
 	attr_accessible :body
 	
 	belongs_to :profile
+	
+	# Define maximum length of each string or text attribute in a publicly accessible way.
+	# This allows them to be used at the view layer for character counts in input and textarea tags.
+	MAX_LENGTHS = {
+		body: 1000
+	}
+	
+	validates :body, length: {maximum: MAX_LENGTHS[:body]}
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
 describe Review do
+	let(:review) { Review.new }
+	
 	it "has a body attribute" do
-		review = Review.new
 		review.body = 'Luciano Pavarotti can sing!'
 		review.should have(:no).errors_on(:body)
+	end
+	
+	it "limits the number of input characters for attributes stored as string or text records" do
+		[:body].each do |attr|
+			s = 'a' * Review::MAX_LENGTHS[attr]
+			review.send "#{attr}=", s
+			review.should have(:no).errors_on(attr)
+			review.send "#{attr}=", (s + 'a')
+			review.should have(1).error_on(attr)
+		end
 	end
 end
