7ce64e867a4df34fdd7d7851bc3e852a8c32d2b5
Edward Simpson <edsimpson@gmail.com>
2013-08-24 23:57:11 -0700

    Created helper for linking to a user's profile.
    Use the helper to display the user's name and link to their profile, if
    any, when showing reviews.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,4 +10,17 @@ module UsersHelper
 			'No profile!'
 		end
 	end
+	
+	def user_display_name(user)
+		user.username.presence || user.profile.try(:display_name_or_company).presence || t('views.user.view.no_name')
+	end
+	
+	def user_profile_path(user)
+		profile = user.profile
+		profile && can?(:show, profile) ? profile_path(profile) : '#'
+	end
+	
+	def user_profile_link(user)
+		link_to user_display_name(user), user_profile_path(user)
+	end
 end
