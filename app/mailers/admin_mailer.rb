7edf0418b2d4b520b5e3f39815a889d4e743452e
bershika <bershika@gmail.com>
2014-04-21 22:01:27 -0700

    Sendgrid gem added and configured for Sendgrid analytics

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,4 +1,5 @@
 class AdminMailer < ActionMailer::Base
+	include SendGrid #Setup custom X- header for sendgrid
 	helper :mailer # Access to MailerHelper methods in this mailer's views.
 	
 	default from: MAILER_DEFAULT_FROM,
@@ -8,18 +9,22 @@ class AdminMailer < ActionMailer::Base
 	# Notify profile moderator, when profile is updated.
 	def on_update_alert(profile)
 		@profile = profile
-		mail
+		sendgrid_category 'Profile Update Alert'
+		sendgrid_unique_args :profile_id => @profile.id
+		mail 
 	end
 	
 	# Notify site admin, when a provider registers.
 	def provider_registration_alert(user)
 		@user, @profile = user, user.profile
+		sendgrid_category 'Provider Registration Alert'
 		mail subject: "Provider \"#{user.email}\" has registered", to: ADMIN_EMAIL
 	end
 	
 	# Notify site admin, when a parent registers.
 	def parent_registration_alert(user)
 		@user = user
+		sendgrid_category 'Parent Registration Alert'
 		mail subject: "Parent \"#{user.email}\" has registered", to: ADMIN_EMAIL
 	end
 end
--- app/mailers/profile_mailer.rb
+++ app/mailers/profile_mailer.rb
@@ -1,10 +1,13 @@
 class ProfileMailer < ActionMailer::Base
+	include SendGrid #Allows custom X- headers for sendgrid
 	helper :mailer # Access to MailerHelper methods in this mailer's views.
 	layout 'user_account_mailer'
 	default from: MAILER_DEFAULT_FROM
+	sendgrid_category :use_subject_lines # Set sendgrid category to email subject
 	
 	def invite(profile)
 		@profile = profile
-		mail to: profile.invitation_email, from: 'Jim Scott <jscott@kinstantly.com>', category: 'profile_invite'
+		sendgrid_unique_args :profile_id => @profile.id
+		mail to: profile.invitation_email, from: 'Jim Scott <jscott@kinstantly.com>'
 	end
 end
