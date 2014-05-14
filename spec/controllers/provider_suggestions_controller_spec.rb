98d47b2a44515b2b09128771166f2069d68aa5eb
Edward Simpson <edsimpson@gmail.com>
2014-05-14 02:43:14 -0700

    More specs for provider_suggestions.
    provider_suggestion Rspec:
    * Added specs for modifying protected attributes.
    * DRYed up provider_suggestion creation.

--- spec/controllers/provider_suggestions_controller_spec.rb
+++ spec/controllers/provider_suggestions_controller_spec.rb
@@ -5,6 +5,8 @@ describe ProviderSuggestionsController do
 	# This should return at least the minimal set of attributes required to create a valid ProviderSuggestion.
 	let (:valid_attributes) { FactoryGirl.attributes_for :provider_suggestion }
 
+	let(:provider_suggestion) { FactoryGirl.create :provider_suggestion }
+
 	context "not signed in" do
 		describe "GET new" do
 			it "assigns a new provider_suggestion as @provider_suggestion" do
@@ -48,6 +50,21 @@ describe ProviderSuggestionsController do
 					response.should render_template('create')
 				end
 			end
+			
+			describe "attempting to modify protected attribute(s)" do
+				it "cannot assign admin notes" do
+					expect {
+						post :create, provider_suggestion: valid_attributes.merge(admin_notes: 'Sneaky notes.')
+					}.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+				end
+
+				it "cannot assign the suggester" do
+					suggester = FactoryGirl.create :parent
+					expect {
+						post :create, provider_suggestion: valid_attributes.merge(suggester_id: suggester.to_param)
+					}.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
+				end
+			end
 		end
 	end
 
@@ -55,12 +72,12 @@ describe ProviderSuggestionsController do
 		let(:admin_user) { FactoryGirl.create :admin_user, email: 'iris@example.com' }
 
 		before (:each) do
+			provider_suggestion # Make sure a persistent provider_suggestion exists.
 			sign_in admin_user
 		end
 
 		describe "GET index" do
 			it "assigns all provider_suggestions as @provider_suggestions" do
-				provider_suggestion = FactoryGirl.create :provider_suggestion
 				get :index
 				assigns(:provider_suggestions).should eq([provider_suggestion])
 			end
@@ -68,7 +85,6 @@ describe ProviderSuggestionsController do
 
 		describe "GET show" do
 			it "assigns the requested provider_suggestion as @provider_suggestion" do
-				provider_suggestion = FactoryGirl.create :provider_suggestion
 				get :show, {:id => provider_suggestion.to_param}
 				assigns(:provider_suggestion).should eq(provider_suggestion)
 			end
@@ -76,7 +92,6 @@ describe ProviderSuggestionsController do
 
 		describe "GET edit" do
 			it "assigns the requested provider_suggestion as @provider_suggestion" do
-				provider_suggestion = FactoryGirl.create :provider_suggestion
 				get :edit, {:id => provider_suggestion.to_param}
 				assigns(:provider_suggestion).should eq(provider_suggestion)
 			end
@@ -85,23 +100,20 @@ describe ProviderSuggestionsController do
 		describe "PUT update" do
 			describe "with valid params" do
 				it "updates the requested provider_suggestion" do
-					provider_suggestion = FactoryGirl.create :provider_suggestion
 					# Assuming there are no other provider_suggestions in the database, this
-					# specifies that the ProviderSuggestion created on the previous line
+					# specifies that the newly created ProviderSuggestion
 					# receives the :update_attributes message with whatever params are
-					# submitted in the request.
-					ProviderSuggestion.any_instance.should_receive(:update_attributes).with({'these' => 'params'})
+					# submitted in the request as the admin role.
+					ProviderSuggestion.any_instance.should_receive(:update_attributes).with({'these' => 'params'}, {as: :admin})
 					put :update, {:id => provider_suggestion.to_param, :provider_suggestion => {'these' => 'params'}}
 				end
 
 				it "assigns the requested provider_suggestion as @provider_suggestion" do
-					provider_suggestion = FactoryGirl.create :provider_suggestion
 					put :update, {:id => provider_suggestion.to_param, :provider_suggestion => valid_attributes}
 					assigns(:provider_suggestion).should eq(provider_suggestion)
 				end
 
 				it "redirects to the provider_suggestion" do
-					provider_suggestion = FactoryGirl.create :provider_suggestion
 					put :update, {:id => provider_suggestion.to_param, :provider_suggestion => valid_attributes}
 					response.should redirect_to(provider_suggestion)
 				end
@@ -109,7 +121,6 @@ describe ProviderSuggestionsController do
 
 			describe "with invalid params" do
 				it "assigns the provider_suggestion as @provider_suggestion" do
-					provider_suggestion = FactoryGirl.create :provider_suggestion
 					# Trigger the behavior that occurs when invalid params are submitted
 					ProviderSuggestion.any_instance.stub(:save).and_return(false)
 					put :update, {:id => provider_suggestion.to_param, :provider_suggestion => {}}
@@ -117,25 +128,30 @@ describe ProviderSuggestionsController do
 				end
 
 				it "re-renders the 'edit' template" do
-					provider_suggestion = FactoryGirl.create :provider_suggestion
 					# Trigger the behavior that occurs when invalid params are submitted
 					ProviderSuggestion.any_instance.stub(:update_attributes).and_return(false)
 					put :update, {:id => provider_suggestion.to_param, :provider_suggestion => {}}
 					response.should render_template("edit")
 				end
 			end
+			
+			describe "admin can modify attribute(s) that are protected from the default role" do
+				it "can assign admin notes" do
+					expect {
+						put :update, id: provider_suggestion.to_param, provider_suggestion: {admin_notes: 'Good notes.'}
+					}.to_not raise_error(ActiveModel::MassAssignmentSecurity::Error)
+				end
+			end
 		end
 
 		describe "DELETE destroy" do
 			it "destroys the requested provider_suggestion" do
-				provider_suggestion = FactoryGirl.create :provider_suggestion
 				expect {
 					delete :destroy, {:id => provider_suggestion.to_param}
 				}.to change(ProviderSuggestion, :count).by(-1)
 			end
 
 			it "redirects to the provider_suggestions list" do
-				provider_suggestion = FactoryGirl.create :provider_suggestion
 				delete :destroy, {:id => provider_suggestion.to_param}
 				response.should redirect_to(provider_suggestions_url)
 			end
