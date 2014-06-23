7905f704b7187c3e996cbc21b3eb58d665b4eb87
Edward Simpson <edsimpson@gmail.com>
2014-06-23 00:20:04 -0700

    Fixed subcategories request spec.
    Test for user session state, i.e., not logged in or logged in as an
    administrator.

--- spec/requests/subcategories_spec.rb
+++ spec/requests/subcategories_spec.rb
@@ -1,10 +1,28 @@
 require 'spec_helper'
 
 describe "Subcategories" do
-	describe "GET /subcategories" do
-		it "has an index page" do
-			get subcategories_path
-			response.status.should be(200)
+	context "not logged in" do
+		describe "GET /subcategories" do
+			it "index page should be inaccessible to non-administrators" do
+				get subcategories_path
+				response.status.should be(302)
+			end
+		end
+	end
+
+	context "logged in as an administrator" do
+		let(:admin_user) { FactoryGirl.create :admin_user }
+		
+		before (:each) do
+			# sign in as an administrator
+			post user_session_path, user: { email: admin_user.email, password: admin_user.password }
+		end
+		
+		describe "GET /subcategories" do
+			it "has an index page" do
+				get subcategories_path
+				response.status.should be(200)
+			end
 		end
 	end
 end
