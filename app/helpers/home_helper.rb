6a5af960d8702ce1ac73d4c6dc62f1024d2ba1a4
Edward Simpson <edsimpson@gmail.com>
2014-07-08 18:55:00 -0700

    Revert home column helper for redesign.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,7 @@
 module HomeHelper
 	def home_column_categories(column)
 		# CategoryList.home_list.categories.where(home_page_column: column).display_order.order_by_name
-		Category.where(home_page_column: (column < 2 ? [1, 2] : [3, 4, 5])).home_page_order
+		Category.where(home_page_column: column).home_page_order
 	end
 	
 	def show_all_column_categories(column)
