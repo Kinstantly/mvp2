2accbe753fc25d58782c21991add6f0f302dbc24
Edward Simpson <edsimpson@gmail.com>
2014-08-09 01:47:22 -0700

    Fix the home page view spec.
    Pretty much all of the home page view is in content_for blocks except
    for the category list.  So that's all that will render in a view spec,
    apparently.

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 describe "home/index" do
-	it "shows the home page headline" do
+	it "shows the home page categories" do
+		['Activities', 'Education', 'Family Services', 'Health', 'Therapy & Coaching'].each do |name|
+			FactoryGirl.create :predefined_category, name: name
+		end
 		render
-		rendered.should have_content(company_name)
+		CategoryList.home_list.categories.each do |category|
+			rendered.should have_content(category.name)
+		end
 	end
 end
