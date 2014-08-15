d99a94285d8787484bff4cbee4cf23048a78e3c5
Edward Simpson <edsimpson@gmail.com>
2014-08-14 23:06:58 -0700

    Label web-hook test logging.

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -5,7 +5,7 @@ class StripeController < ApplicationController
 	def webhook
 		event = JSON.parse(request.body.read)
 		logger.debug "event = #{event}" if Rails.env == 'development'
-		logger.info "provider_id=>\"#{params[:provider_id]}\"; type=>\"#{event['type']}\"; user_id=>\"#{event['user_id']}\"; amount=>\"#{event['data']['object']['amount']}\"; "
+		logger.info "webhook: provider_id=>\"#{params[:provider_id]}\"; type=>\"#{event['type']}\"; user_id=>\"#{event['user_id']}\"; amount=>\"#{event['data']['object']['amount']}\"; "
 		# response.status = 200
 		render json: {}
 	end
