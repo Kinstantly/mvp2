d737473ffbb063a79e98d6552cacbd9ccb2109cc
Edward Simpson <edsimpson@gmail.com>
2014-08-17 22:50:58 -0700

    Add test web-hook logging for ti.to.

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -4,8 +4,13 @@ class StripeController < ApplicationController
 	
 	def webhook
 		event = JSON.parse(request.body.read)
+		provider_id = params[:provider_id]
 		logger.debug "event = #{event}" if Rails.env == 'development'
-		logger.info "webhook: provider_id=>\"#{params[:provider_id]}\"; type=>\"#{event['type']}\"; user_id=>\"#{event['user_id']}\"; amount=>\"#{event['data']['object']['amount']}\"; "
+		if provider_id == 'hank'
+			logger.info "webhook: provider_id=>\"#{provider_id}\"; type=>\"#{event['type']}\"; user_id=>\"#{event['user_id']}\"; amount=>\"#{event['data']['object']['amount']}\""
+		else
+			logger.info "webhook: provider_id=>\"#{provider_id}\"; price=>\"#{event['price']}\"; custom=>\"#{event['custom']}\""
+		end
 		# response.status = 200
 		render json: {}
 	end
