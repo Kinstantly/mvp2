c6655c68c8e81f0dcdf071e0937a6603df18b6e5
Edward Simpson <edsimpson@gmail.com>
2014-08-26 17:33:19 -0700

    More logging of Stripe web-hook events.

--- app/controllers/stripe_controller.rb
+++ app/controllers/stripe_controller.rb
@@ -5,19 +5,33 @@ class StripeController < ApplicationController
 	def webhook
 		event = JSON.parse(request.body.read)
 		provider_id = params[:provider_id].try(:to_s)
-		logger.debug "event = #{event.to_s}" if Rails.env == 'development'
-		if not event.is_a?(Hash)
-			logger.info "webhook: provider_id=>\"#{provider_id}\"; event is a #{event.class}"
-		elsif provider_id == 'hank' and event['type'] == 'charge.succeeded'
-			event.delete('id')
-			event['data'].try(:'[]', 'object').try(:delete, 'id')
-			event['data'].try(:'[]', 'object').try(:delete, 'card')
-			event['data'].try(:'[]', 'object').try(:delete, 'balance_transaction')
-			logger.info "webhook: provider_id=>\"#{provider_id}\"; event=>#{event.to_s}"
+		logger.debug "event=>#{event.to_s}" if Rails.env == 'development'
+		if event.is_a?(Hash)
+			type = event['type']
+			msgs = []
+			add_property msgs, 'provider_id', provider_id
+			add_property msgs, 'type', type
+			add_property msgs, 'user_id', event['user_id']
+			data = event['data']
+			object = data.try(:'[]', 'object')
+			case type
+			when 'charge.succeeded'
+				add_property msgs, 'amount', object['amount']
+				add_property msgs, 'customer', object['customer']
+			when 'customer.created'
+				add_property msgs, 'customer_id', object['id']
+			end
+			logger.info('Webhook: ' + msgs.join('; '))
 		else
-			logger.info "webhook: provider_id=>\"#{provider_id}\"; event['type']=>#{event['type']}"
+			logger.info "Webhook: provider_id=>\"#{provider_id}\"; event is a #{event.class}"
 		end
 		# response.status = 200
 		render json: {}
+	end
+	
+	private
+	
+	def add_property(messages, name, value)
+		messages << "#{name}=>\"#{value}\""
 	end
 end
