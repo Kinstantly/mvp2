719ee36ca93e967a678d536bfdac7a2892ab7197
Edward Simpson <edsimpson@gmail.com>
2014-09-10 21:48:25 -0700

    Basic Rspec for Stripe models.

--- spec/models/stripe_card_spec.rb
+++ spec/models/stripe_card_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe StripeCard do
-	pending "add some examples to (or delete) #{__FILE__}"
+	let(:stripe_card) { FactoryGirl.create :stripe_card }
+	
+	it "has an API ID" do
+		stripe_card.api_card_id.should be_present
+	end
+	
+	it "can have a charge" do
+		expect {
+			stripe_card.stripe_charges << FactoryGirl.create(:stripe_charge)
+		}.to change { stripe_card.stripe_charges.size }.by(1)
+	end
+	
+	it "can have multiple charges" do
+		expect {
+			stripe_card.stripe_charges << FactoryGirl.create(:stripe_charge)
+			stripe_card.stripe_charges << FactoryGirl.create(:stripe_charge)
+		}.to change { stripe_card.stripe_charges.size }.by(2)
+	end
 end
--- spec/models/stripe_charge_spec.rb
+++ spec/models/stripe_charge_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe StripeCharge do
-	pending "add some examples to (or delete) #{__FILE__}"
+	let(:stripe_charge) { FactoryGirl.create :stripe_charge }
+	let(:captured_stripe_charge) { FactoryGirl.create :captured_stripe_charge }
+	let(:uncaptured_stripe_charge) { FactoryGirl.create :uncaptured_stripe_charge }
+	let(:refunded_stripe_charge) { FactoryGirl.create :refunded_stripe_charge }
+	let(:partially_refunded_stripe_charge) { FactoryGirl.create :partially_refunded_stripe_charge }
+	
+	it "has an API ID" do
+		stripe_charge.api_charge_id.should be_present
+	end
+	
+	it "has a charge amount" do
+		captured_stripe_charge.amount.should be > 0
+		captured_stripe_charge.paid.should be_true
+	end
+	
+	it "can be captured" do
+		captured_stripe_charge.captured.should be_true
+	end
+	
+	it "can be uncaptured" do
+		uncaptured_stripe_charge.captured.should be_false
+	end
+	
+	it "can be refunded" do
+		refunded_stripe_charge.amount_refunded.should be > 0
+		refunded_stripe_charge.refunded.should be_true
+	end
+	
+	it "can be partially refunded" do
+		partially_refunded_stripe_charge.amount_refunded.should be < partially_refunded_stripe_charge.amount
+		partially_refunded_stripe_charge.refunded.should be_false
+	end
 end
