5585d18f3541d95e6bc34ce765abc60a952e52c1
bershika <bershika@gmail.com>
2014-12-03 23:19:51 -0800

    Signup via iframe on the blog site

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 	# Add this controller to the devise route if you need to customize the registration controller.
-	
+
 	before_filter :before_registration, only: :create
 	after_filter :after_registration, only: :create
 	
@@ -9,12 +9,40 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 	layout :registrations_layout
 
+	def in_blog_new
+		new
+	end
+
 	def edit
 		if params[:contact_preferences].present?
 			# The fragment ID is not preserved across a sign-in, so fake it with a query parameter.  :(
 			redirect_to(edit_user_registration_url + '#contact_preferences')
 		else
 			super
+		end
+	end
+
+	# POST /resource
+	def create
+		build_resource(sign_up_params)
+
+		if resource.save
+			if resource.active_for_authentication?
+				set_flash_message :notice, :signed_up if is_navigational_format?
+				sign_up(resource_name, resource)
+				respond_with resource, :location => after_sign_up_path_for(resource)
+			else
+				set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+				expire_session_data_after_sign_in!
+				respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+			end
+		else
+			clean_up_passwords resource
+			if params[:in_blog].present?
+				render :in_blog_new
+			else
+				render :new
+			end
 		end
 	end
 
@@ -99,10 +127,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	end
 
 	def after_inactive_sign_up_path_for(resource)
-		member_awaiting_confirmation_path
+		if params[:in_blog].present?
+			in_blog_awaiting_confirmation_path
+		else
+			member_awaiting_confirmation_path
+		end
 	end
-	
+
+	def after_sign_up_error_path_for(resource)
+		if params[:in_blog].present?
+			in_blog_sign_up_path(resource)
+		else
+			new_user_registration_path(resource)
+		end
+	end
+
 	def registrations_layout
-		['new', 'create'].include?(action_name) ? 'interior' : 'interior'
+		# Use iframe_layout if in_blog present (i.e. unsuccessful create action with in-blog signup)
+		return 'iframe_layout' if params[:in_blog].present?
+		['in_blog_new', 'in_blog_awaiting_confirmation'].include?(action_name) ? 'iframe_layout' : 'interior'
 	end
 end
