d2a0155aefa43a6d66eed78161cd755a11b4b171
Edward Simpson <edsimpson@gmail.com>
2014-12-24 03:04:51 -0800

    Touch profile when Stripe Connect changes.
    If a provider signs up for Stripe Connect, updates their Stripe Connect
    info, or discontinues it, ensure that any fragment cache(s) for the
    profile page will be updated.

--- app/models/stripe_info.rb
+++ app/models/stripe_info.rb
@@ -4,5 +4,15 @@ class StripeInfo < ActiveRecord::Base
 	belongs_to :user
 	has_many :stripe_customers
 	
+	after_save :touch_profile
+	after_destroy :touch_profile
+	
 	validates_presence_of :stripe_user_id, :access_token, :publishable_key
+	
+	private
+	
+	# Touch profile to invalidate fragment cache(s) in case we need to update payment info on the profile page.
+	def touch_profile
+		user.try(:profile).try(:touch)
+	end
 end
