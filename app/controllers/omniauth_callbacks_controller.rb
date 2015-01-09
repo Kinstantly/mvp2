1b77c5296f722e31378772775e354d16107f2987
Edward Simpson <edsimpson@gmail.com>
2015-01-08 23:21:06 -0800

    Encapsulated StripeInfo configuration.
    Moved configuration of StripeInfo into the model.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,14 +5,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 	def stripe_connect
 		auth = request.env["omniauth.auth"]
-		stripe_user_id = auth.try(:[], :uid)
-		access_token = auth.try(:[], :credentials).try(:[], :token)
-		publishable_key = auth.try(:[], :extra).try(:[], :raw_info).try(:[], :stripe_publishable_key)
 		stripe_info = StripeInfo.find_or_initialize_by_user_id(current_user.id)
-		stripe_info.stripe_user_id = stripe_user_id
-		stripe_info.access_token = access_token
-		stripe_info.publishable_key = publishable_key
-		if stripe_info.save!
+		if stripe_info.configure_authorization(auth)
 			render 'success'
 		else
 			logger.error "Stripe Connect error for user #{current_user.id}. stripe_info.errors => #{stripe_info.errors.full_messages}. Omniauth Params => #{auth}"
--- app/models/stripe_info.rb
+++ app/models/stripe_info.rb
@@ -9,6 +9,14 @@ class StripeInfo < ActiveRecord::Base
 	
 	validates_presence_of :stripe_user_id, :access_token, :publishable_key
 	
+	# Accepts authorization values obtained via the "omniauth.auth" request header.
+	def configure_authorization(auth_values)
+		self.stripe_user_id = auth_values.try(:[], :uid)
+		self.access_token = auth_values.try(:[], :credentials).try(:[], :token)
+		self.publishable_key = auth_values.try(:[], :extra).try(:[], :raw_info).try(:[], :stripe_publishable_key)
+		save
+	end
+	
 	private
 	
 	# Touch profile to invalidate fragment cache(s) in case we need to update payment info on the profile page.
