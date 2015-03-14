2e22ccebb7f6bef7bbe391ec279c313eb296e6ab
Edward Simpson <edsimpson@gmail.com>
2015-03-13 22:36:14 -0700

    Lower case button text in payment announcement.

--- app/models/announcement/payment_profile_announcement.rb
+++ app/models/announcement/payment_profile_announcement.rb
@@ -4,7 +4,7 @@ class PaymentProfileAnnouncement < ProfileAnnouncement
 		position: 1,
 		headline: 'Pay me online',
 		body: 'I accept online payments through Kinstantly. Please click the button to get started&mdash;or to learn more about how it works.',
-		button_text: 'Pay now',
+		button_text: 'pay now',
 		search_result_position: 1,
 		search_result_link_text: 'I accept online payments'
 	}
