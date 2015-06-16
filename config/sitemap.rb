c1b4c92a496957e558c82df243a88143a05152f5
Edward Simpson <edsimpson@gmail.com>
2015-06-15 21:52:56 -0700

    Added newsletter-related URLs.
    Also, added the parent sign-up page.
    Lowered priority of profile pages to favor the newsletter sign-up page.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -9,13 +9,28 @@ SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
 
 SitemapGenerator::Sitemap.create do
 	add provider_sign_up_path, changefreq: 'monthly'
+	add new_user_registration_path, changefreq: 'monthly'
 	add about_path, changefreq: 'monthly'
 	add contact_path, changefreq: 'monthly'
 	add terms_path, changefreq: 'monthly'
 	add privacy_path, changefreq: 'monthly'
 	
 	Profile.where(is_published: true).find_each do |profile|
-		add profile_path(profile), priority: 1.0, changefreq: 'daily', lastmod: profile.updated_at
+		add profile_path(profile), priority: 0.8, changefreq: 'daily', lastmod: profile.updated_at
+	end
+	
+	# Newsletter sign-up.
+	# Because this page displays the latest samples, it changes with each mailing.
+	add newsletter_signup_path, priority: 1.0, changefreq: 'weekly'
+	
+	# Newsletter archive.
+	# Because the list page displays links to all samples, it changes with each mailing.
+	add newsletter_list_path, changefreq: 'weekly'
+	# And a page for each campaign (mailing).
+	[:parent_newsletters_stage1, :parent_newsletters_stage2, :parent_newsletters_stage3].each do |list|
+		Newsletter.send(list).each do |newsletter|
+			add newsletter_path(newsletter.cid), changefreq: 'monthly', lastmod: newsletter.updated_at if newsletter.content.present?
+		end
 	end
 	
 	# Put links creation logic here.
