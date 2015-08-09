4fbd677d904637a5ea6b30d3ccfb5bf64b927be2
Edward Simpson <edsimpson@gmail.com>
2015-08-08 22:53:52 -0700

    View spec should expect new About Us page.

--- spec/views/home/about.html.haml_spec.rb
+++ spec/views/home/about.html.haml_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe "home/about", :type => :view do
 	it "should have some text about us" do
 		render
-		expect(rendered).to have_content 'Meet the Founders'
+		expect(rendered).to have_content 'Our Story'
 	end
 end
