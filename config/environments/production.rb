0b15cc606a3901125bd7a6a8dfc5c5a294ee3b8a
Edward Simpson <edsimpson@gmail.com>
2015-11-10 00:25:19 -0800

    Precompile new prototype stylesheets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Mvp2::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( home_page.css interior.css home_page_ie8.css plain.css ie.css )
+  config.assets.precompile += %w( home_page_version_a.css home_page_version_b.css ) # Prototypes.
 
   # Precompile additional js manifests
   config.assets.precompile += %w( profile_edit.js profile_search.js profile_show.js review_new.js payment.js )
