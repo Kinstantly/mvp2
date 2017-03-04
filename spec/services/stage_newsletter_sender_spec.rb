fb17aafddf2538e231f0717d0df6129477e00864
Edward Simpson <edsimpson@gmail.com>
2017-03-03 23:09:59 -0800

    Refactored some stage-based newsletter sender specs

--- spec/services/stage_newsletter_sender_spec.rb
+++ spec/services/stage_newsletter_sender_spec.rb
@@ -165,80 +165,91 @@ describe StageNewsletterSender, mailchimp: true, use_gibbon_mocks: true do
 				end
 			end
 			
-			it 'should deliver to many recipients' do
-				# use a smallish batch size to test multi-batch retrieval
-				newsletter_sender_params[:batch_size] = 10
+			context 'with many recipients' do
+				# override member_emails and members; they are used by the setup procedures
+				let(:n_members) { 102 }
+				let(:member_emails) {
+					(1..n_members).inject([]) do |emails, i|
+						emails << "subscriber_#{i}@kinstantly.com"
+					end
+				}
+				let(:members) {
+					days_since_birth = now - (stages[titles[0]] + 2).days
+					
+					member_emails.inject({}) do |members_hash, email|
+						members_hash[email] = [days_since_birth]
+						members_hash
+					end
+				}
 				
-				# seed members; it is used by the setup procedures
-				days_since_birth = now - (stages[titles[0]] + 2).days
-				member_emails.delete_if { |email| true } # clear it out
-				members # should be empty now
-				for i in 1..102 do
-					email = "subscriber_#{i}@kinstantly.com"
-					member_emails << email
-					members[email] = [days_since_birth]
+				it 'should successfully schedule, then use multiple batches to retrieve the scheduled members' do
+					# use a smallish batch size to test multi-batch retrieval
+					newsletter_sender_params[:batch_size] = 10
+					
+					create_subscriptions
+					create_stages
+				
+					expect {
+						newsletter_sender.call
+					}.to change(SubscriptionDelivery, :count).by n_members
 				end
-				
-				create_subscriptions
-				create_stages
-				
-				expect {
-					newsletter_sender.call
-				}.to change(SubscriptionDelivery, :count).by members.size
 			end
 			
-			it "should use any of the children's birth dates for recipient selection" do
+			context 'with recipients that have multiple children' do
 				# array of days since birth for each stage
-				days_since_birth = (0..2).inject([]) do |days, i|
-					days << (stages[titles[i]] + 1).days
-				end
+				let(:days_since_birth) {
+					(0..2).inject([]) do |days, i|
+						days << (stages[titles[i]] + 1).days
+					end
+				}
 				
+				# override member_emails and members; they are used by the setup procedures
+				let(:member_emails) {
+					(1..4).inject([]) do |emails, i|
+						emails << "subscriber_#{i}@kinstantly.com"
+					end
+				}
 				# seed with members that should receive a stage based on the second, third, or fourth child
+				let(:members) {
+					members_hash = {}
+					members_hash[member_emails[0]] = [
+						now + 30.days, now - days_since_birth[0]
+					]
+					members_hash[member_emails[1]] = [
+						now + 30.days, nil, now - days_since_birth[1]
+					]
+					members_hash[member_emails[2]] = [
+						now + 30.days, nil, nil, now - days_since_birth[2]
+					]
+					members_hash[member_emails[3]] = [
+						now + 30.days, now - days_since_birth[0], now - days_since_birth[1]
+					]
+					members_hash
+				}
 				
-				member_emails.delete_if { |email| true } # clear it out
-				members # should be empty now
+				it "should use any of the children's birth dates for recipient selection" do
+					create_subscriptions
+					create_stages
 				
-				member_emails << (email = 'subscriber_1@kinstantly.com')
-				members[email] = [
-					now + 30.days, now - days_since_birth[0]
-				]
+					expect {
+						newsletter_sender.call
+					}.to change(SubscriptionDelivery, :count).by 5
 				
-				member_emails << (email = 'subscriber_2@kinstantly.com')
-				members[email] = [
-					now + 30.days, nil, now - days_since_birth[1]
-				]
+					stage_delivery_count = SubscriptionDelivery.where({
+						subscription_stage: SubscriptionStage.find_by_title(titles[0])
+					}).count
+					expect(stage_delivery_count).to eq 2
 				
-				member_emails << (email = 'subscriber_3@kinstantly.com')
-				members[email] = [
-					now + 30.days, nil, nil, now - days_since_birth[2]
-				]
+					stage_delivery_count = SubscriptionDelivery.where({
+						subscription_stage: SubscriptionStage.find_by_title(titles[1])
+					}).count
+					expect(stage_delivery_count).to eq 2
 				
-				member_emails << (email = 'subscriber_4@kinstantly.com')
-				members[email] = [
-					now + 30.days, now - days_since_birth[0], now - days_since_birth[1]
-				]
-				
-				create_subscriptions
-				create_stages
-				
-				expect {
-					newsletter_sender.call
-				}.to change(SubscriptionDelivery, :count).by 5
-				
-				stage_delivery_count = SubscriptionDelivery.where({
-					subscription_stage: SubscriptionStage.find_by_title(titles[0])
-				}).count
-				expect(stage_delivery_count).to eq 2
-				
-				stage_delivery_count = SubscriptionDelivery.where({
-					subscription_stage: SubscriptionStage.find_by_title(titles[1])
-				}).count
-				expect(stage_delivery_count).to eq 2
-				
-				stage_delivery_count = SubscriptionDelivery.where({
-					subscription_stage: SubscriptionStage.find_by_title(titles[2])
-				}).count
-				expect(stage_delivery_count).to eq 1
+					stage_delivery_count = SubscriptionDelivery.where({
+						subscription_stage: SubscriptionStage.find_by_title(titles[2])
+					}).count
+					expect(stage_delivery_count).to eq 1
+				end
 			end
 			
 			it 'should not send duplicates' do
