d2d140a9edbb364cd5b4ed5211f63e5160b88fe0
Edward Simpson <edsimpson@gmail.com>
2017-05-14 01:23:48 -0700

    Embed profile email captcha in an overlay
    Instead of using a pop-up window for the email captcha on the profile
    page, embed the captcha iframe in an overlay.

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -101,12 +101,14 @@ module ProfilesHelper
 		end
 	end
 
-	def profile_captcha_email(profile=current_user.try(:profile), title=nil)
+	def profile_captcha_email(profile=current_user.try(:profile), options={})
 		if (email = profile.try(:email)).present?
 			hidden_email = truncate profile_obscured_email(profile), length: 24
-			mailhide_link = RecaptchaMailhide::URL.url_for(email)
-			js_click_event = "window.open(this.href, '#{title}', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300,left=200,top=200'); return false;"
-			link_to hidden_email, mailhide_link, { title: title, onclick: js_click_event }
+			mailhide_link = RecaptchaMailhide.url_for(email)
+			title = options[:title]
+			target = options[:target]
+			onclick = options[:onclick] || "window.open(this.href, '#{title}', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300,left=200,top=200'); return false;"
+			link_to hidden_email, mailhide_link, title: title, target: target, onclick: onclick
 		end
 	end
 
