c70474fa8a4b8b7c89d169e8c8ea484fdd02db10
Edward Simpson <edsimpson@gmail.com>
2018-01-01 22:57:56 -0800

    More configuration clean-up

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -7,7 +7,7 @@
 # no regular words or you'll be exposed to dictionary attacks.
 # You can use `rake secret` to generate a secure secret key.
 
-Mvp2::Application.config.secret_token = 'c00622ad01f871609998b1fe3a615668cfe57eb2da29c353ddce7f65a8e25d02b1c58260dae8a7b35917e5cc6f3e899bed97da723bf2ab6a05d5bd1e3fa001b4'
+Mvp2::Application.config.secret_token = 'db35eb96f31ccf6c55744bd90c1533e92a97a89b4bfbe90938b92b7098a931ad93a395409e04361897fc47add0d8175b8c753e0a4d7036a3aeb302517fcb013b'
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
